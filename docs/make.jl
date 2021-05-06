55f1f85010195f7f631aa61de4489e03dfa6f14a
hdavid16 <hperez16@gmail.com>
2021-05-05 22:54:39 -0400

    workflows

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,12 @@
-using Documenter, JuGDP
-
-makedocs(sitename="Julia Generalized Disjunctive Programming",
-         pages = Any["About" => "index.md"])
+push!(LOAD_PATH,"../src/")
+using JuGDP
+using Documenter
+makedocs(
+         sitename = "JuGDP.jl",
+         modules  = [JuGDP],
+         pages=[
+                "Home" => "index.md"
+               ])
+deploydocs(;
+    repo="github.com/hdavid16/JuGDP.jl",
+)
