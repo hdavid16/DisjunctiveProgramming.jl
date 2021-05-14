5eff89af5b350a988cb31d1a0021bfa1645dc3e9
Documenter.jl <documenter@juliadocs.github.io>
2021-05-14 19:56:10 +0000

    build based on fd700d9

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Overview","page":"Home","title":"Overview","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"DisjunctiveProgramming.jl","category":"page"}]
+}
