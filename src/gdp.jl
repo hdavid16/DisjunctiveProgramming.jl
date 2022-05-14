758f418da2e37c9276fb23a7b4e1b063f69fc4d0
hdavid16 <hperez16@gmail.com>
2022-05-14 11:27:14 -0400

    add structs

--- src/gdp.jl
+++ src/gdp.jl
@@ -1,3 +1,12 @@
+mutable struct Disjunct
+    constraints::Vector{AbstractConstraint}
+    indicator::VariableRef
+end
+
+mutable struct DisjunctionConstraint <: AbstractConstraint
+    disjuncts::Vector{Disjunct}
+end
+
 mutable struct GDPdata
     gdp_variable_refs::Vector
     gdp_variable_names::Vector
