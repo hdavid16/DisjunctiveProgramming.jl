2460400f4f6f73cd5c22517c22a1f485d2c03741
hdavid16 <hperez16@gmail.com>
2022-09-10 20:15:35 -0400

    clean up @disjunction

--- src/macros.jl
+++ src/macros.jl
@@ -4,43 +4,27 @@
 Add disjunction macro.
 """
 macro disjunction(m, args...)
-    #get disjunction (pos_args) and keyword arguments
-    pos_args, kw_args, _ = Containers._extract_kw_args(args)
-    @assert length(pos_args) > 1 "At least 2 disjuncts must be included. If there is an empty disjunct, use `nothing`."
+    #get disjunction (args) and keyword arguments
+    disjuncts, kwargs, _ = Containers._extract_kw_args(args)
 
-    #get kw_args and set defaults if missing
-    reformulation = filter(i -> i.args[1] == :reformulation, kw_args)
-    if !isempty(reformulation)
-        reformulation = reformulation[1].args[2]
-        reformulation_kind = eval(reformulation)
-        @assert reformulation_kind in [:big_m, :hull] "Invalid reformulation method passed to keyword argument `:reformulation`. Valid options are :big_m (Big-M Reformulation) and :hull (Hull Reformulation)."
-        if reformulation_kind == :big_m
-            M = filter(i -> i.args[1] == :M, kw_args)
-            param = !isempty(M) ? M[1].args[2] : :(missing)
-        elseif reformulation_kind == :hull
-            ϵ = filter(i -> i.args[1] == :ϵ, kw_args)
-            param = !isempty(ϵ) ? ϵ[1].args[2] : :(1e-6)
-        else
-        end
-    else
-        throw(UndefKeywordError(:reformulation))
-    end
-    name = filter(i -> i.args[1] == :name, kw_args)
-    if !isempty(name)
-        name = name[1].args[2]
-        disj_name = Symbol("disj_",eval(name))
-    else
-        name = :(Symbol("disj",gensym()))
-        disj_name = eval(name)
-    end
+    #get kwargs and set defaults if missing
+    reformulation_kwarg = filter(i -> i.args[1] == :reformulation, kwargs)
+    reformulation = isempty(reformulation_kwarg) ? throw(UndefKeywordError(:reformulation)) : reformulation_kwarg[1].args[2]
+    M_kwarg = filter(i -> i.args[1] == :M, kwargs)
+    M = isempty(M_kwarg) ? :missing : M_kwarg[1].args[2]
+    ϵ_kwarg = filter(i -> i.args[1] == :ϵ, kwargs)
+    ϵ = isempty(ϵ_kwarg) ? :(1e-6) : ϵ_kwarg[1].args[2]
+    name_kwarg = filter(i -> i.args[1] == :name, kwargs)
+    name = isempty(name_kwarg) ? Symbol("disj_",gensym()) : name_kwarg[1].args[2]
+    disj_name = isempty(name_kwarg) ? name : Symbol("disj_",eval(name))
     
     #create constraints for each disjunction
-    disj_names = [Symbol("$(disj_name)[$i]") for i in eachindex(pos_args)]
+    disj_names = [Symbol("$(disj_name)[$i]") for i in eachindex(disjuncts)]
     disjunction = []
-    for (d,dname) in zip(pos_args,disj_names)
+    for (d,dname) in zip(disjuncts,disj_names)
         if Meta.isexpr(d, :tuple)
             for (j,di) in enumerate(d.args)
-                i = findfirst(x -> x == d, pos_args)
+                i = findfirst(x -> x == d, disjuncts)
                 dname_j = Symbol("$(disj_name)[$i,$j]")
                 d.args[j] = add_disjunction_constraint(m, di, dname_j)
             end
@@ -49,17 +33,10 @@ macro disjunction(m, args...)
             push!(disjunction, add_disjunction_constraint(m, d, dname))
         end
     end
-
-    # #XOR constraint name
-    # xor_con = Symbol("XOR($disj_name)")
     
     #build disjunction
     code = quote
-        if !in($name, keys(object_dictionary($m)))
-            $m[$name] = @variable($m, [eachindex($disjunction)], Bin, base_name = string($name))
-        end
-        # @constraint($m, $xor_con, sum($m[$name]) == 1)
-        DisjunctiveProgramming.reformulate_disjunction($m, $(disjunction...); bin_var = $name, reformulation = $reformulation, param = $param)
+        DisjunctiveProgramming.add_disjunction!($m, $(disjunction...); reformulation = $reformulation, M = $M, ϵ = $ϵ, name = $name)
     end
 
     return esc(code)
@@ -110,7 +87,7 @@ function add_disjunction!(m::Model,disj...;reformulation::Symbol,M=missing,ϵ=1e
     @assert reformulation in [:big_m, :hull] "Invalid reformulation method passed to keyword argument `:reformulation`. Valid options are :big_m (Big-M Reformulation) and :hull (Hull Reformulation)."
     @assert length(disj) > 1 "At least 2 disjuncts must be included. If there is an empty disjunct, use `nothing`."
 
-    #get kw_args and set defaults if missing
+    #get kwargs and set defaults if missing
     param = reformulation == :big_m ? M : ϵ
     bin_var = ismissing(name) ? Symbol("disj",gensym()) : name
     
