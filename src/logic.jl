da2fef5f4348669f35c641f0338f9e987495ecfa
Hector Perez <hperez16@gmail.com>
2023-07-20 13:24:29 -0400

    complete proposition transformation functions

--- src/logic.jl
+++ src/logic.jl
@@ -2,6 +2,9 @@
 
 """
 function _to_cnf(lexpr::LogicalExpr)
+    #NOTE: some redundant constraints may be created in the process.
+    #   For example A ∨ ¬B ∨ B is always true and is reformulated 
+    #   to the redundant constraint A ≥ 0.
     lexpr |> 
         _eliminate_equivalence |>  
         _eliminate_implication |> 
@@ -223,7 +226,10 @@ function _distribute_and_over_or_right(lexpr::LogicalExpr)
 end
 
 """
+    _flatten(lexpr::LogicalRef)
 
+Flatten netsed OR / AND operators and replace them with their n-ary form.
+For example, ∨(∨(A, B), C) is replaced with ∨(A, B, C).
 """
 function _flatten(lvar::LogicalVariableRef)
     return lvar
@@ -234,7 +240,7 @@ function _flatten(lexpr::LogicalExpr)
         for arg in lexpr.args
             if arg isa LogicalVariableRef
                 push!(nary_args, arg)
-            elseif arg.head == :¬
+            elseif arg.head == :¬ && arg.args[1] isa LogicalVariableRef
                 push!(nary_args, arg)
             elseif arg.head == lexpr.head
                 arg_flat = _flatten(arg)
@@ -254,184 +260,3 @@ function _flatten(lexpr::LogicalExpr)
 
     return new_lexpr
 end
-# function JuMP.flatten(expr::LogicalExpr)
-#     root = LogicalExpr(expr.head, Any[])
-#     nodes_to_visit = Any[(root, arg) for arg in reverse(expr.args)]
-#     while !isempty(nodes_to_visit)
-#         parent, arg = pop!(nodes_to_visit)
-#         if !(arg isa LogicalExpr)
-#             # Not a nonlinear expression, so can use recursion.
-#             push!(parent.args, JuMP.flatten(arg))
-#         elseif parent.head in (:∨, :∧) && arg.head == parent.head
-#             # A special case: the arg can be lifted to an n-ary argument of the
-#             # parent.
-#             for n in reverse(arg.args)
-#                 push!(nodes_to_visit, (parent, n))
-#             end
-#         else
-#             # The default case for nonlinear expressions. Put the args on the
-#             # stack, so that we may walk them later.
-#             for n in reverse(arg.args)
-#                 push!(nodes_to_visit, (arg, n))
-#             end
-#             empty!(arg.args)
-#             push!(parent.args, arg)
-#         end
-#     end
-#     return root
-# end
-
-# """
-#     add_proposition!(m::JuMP.Model, expr::Expr; name::String = "")
-
-# Convert logical proposition expression into conjunctive normal form.
-# """
-# function add_proposition!(m::JuMP.Model, expr::Expr; name::String = "")
-#     if isempty(name)
-#         name = "{$expr}" #get name to register reformulated logical proposition
-#     end
-#     replace_Symvars!(expr, m; logical_proposition = true) #replace all JuMP variables with Symbolic variables
-#     clause_list = to_cnf!(expr)
-#     #replace symbolic variables with JuMP variables and boolean operators with their algebraic counterparts
-#     for clause in clause_list
-#         replace_JuMPvars!(clause, m)
-#         replace_logic_operators!(clause)
-#     end
-#     #generate and simplify JuMP expressions for the lhs of the algebraic constraints
-#     lhs = eval.(clause_list)
-#     drop_zeros!.(lhs)
-#     unique!(lhs)
-#     #generate JuMP constraints for the logical proposition
-#     if length(lhs) == 1
-#         m[Symbol(name)] = @constraint(m, lhs[1] >= 1, base_name = name)
-#     else
-#         m[Symbol(name)] = @constraint(m, [i = eachindex(lhs)], lhs[i] >= 1, base_name = name)
-#     end
-# end
-
-# """
-#     to_cnf!(expr::Expr)
-
-# Convert an expression of symbolic Boolean variables and operators to CNF.
-# """
-# function to_cnf!(expr::Expr)
-#     check_logical_proposition(expr) #check that valid boolean symbols and variables are used in the logical proposition
-#     eliminate_equivalence!(expr) #eliminate ⇔
-#     eliminate_implication!(expr) #eliminmate ⇒
-#     move_negations_inwards!(expr) #expand ¬
-#     clause_list = distribute_and_over_or_recursively!(expr) #distribute ∧ over ∨ recursively
-#     @assert !isempty(clause_list) "Conversion to CNF failed."
-
-#     return clause_list
-# end
-
-# """
-#     check_logical_proposition(expr::Expr)
-
-# Validate logical proposition provided.
-# """
-# function check_logical_proposition(expr::Expr)
-#     #NOTE: this is quick and dirty (uses Suppressor.jl). A more robust approach should traverse the expression tree to verify that only valid boolean symbols and model variables are used.
-#     dump_str = @capture_out dump(expr, maxdepth = typemax(Int)) #caputre dump
-#     dump_arr = split(dump_str,"\n") #split by \n
-#     filter!(i -> occursin("1:",i), dump_arr) #filter only first args in each subexpression
-#     @assert all(occursin.("1: Symbol ",dump_arr)) "Logical expression does not use valid Boolean symbols: ∨, ∧, ¬, ⇒, ⇔."
-#     operator_list = map(i -> split(i, "Symbol ")[2], dump_arr)
-#     @assert isempty(setdiff(operator_list, ["∨", "∧", "¬", "⇒", "⇔", "variables"])) "Logical expression does not use valid model variables or allowed Boolean symbols (∨, ∧, ¬, ⇒, ⇔)."
-# end
-
-
-
-
-
-
-
-# """
-#     distribute_and_over_or!(expr)
-
-# Distribute AND over OR boolean operators.
-# """
-# function distribute_and_over_or!(expr)
-#     if expr isa Expr
-#         if expr.args[1] == :∨
-#             # op = expr.args[1]
-#             A = expr.args[2] #first clause
-#             B = expr.args[3] #second clause
-#             if A isa Expr && A.args[1] == :∧ #first clause has AND
-#                 C = A.args[2] #first subclause
-#                 D = A.args[3] #second subclause
-#                 expr.args[1] = :∧ #flip OR to AND in main expr
-#                 expr.args[2] = :($C ∨ $B)
-#                 expr.args[3] = :($D ∨ $B)
-#             elseif B isa Expr && B.args[1] == :∧ #second clause has AND
-#                 C = B.args[2] #first subclause
-#                 D = B.args[3] #second subclause
-#                 expr.args[1] = :∧ #flip OR to AND in main expr
-#                 expr.args[2] = :($C ∨ $A)
-#                 expr.args[3] = :($D ∨ $A)
-#             end
-#         end
-#         for i in eachindex(expr.args)
-#             expr.args[i] = distribute_and_over_or!(expr.args[i])
-#         end
-#     end
-
-#     return expr
-# end
-
-# """
-#     extract_clauses(expr)
-
-# Extract clauses from conjunctive normal form.
-# """
-# function extract_clauses(expr)
-#     clauses = []
-#     if expr isa Expr
-#         if expr.args[1] == :∨
-#             push!(clauses, expr)
-#         else
-#             for i in 2:length(expr.args)
-#                 push!(clauses, extract_clauses(expr.args[i])...)
-#             end
-#         end
-#     end
-
-#     return clauses
-# end
-
-# """
-#     distribute_and_over_or_recursively!(expr)
-
-# Distribute AND over OR boolean operators recursively throughout the expression tree.
-# """
-# function distribute_and_over_or_recursively!(expr)
-#     distribute_and_over_or!(expr)
-#     clause_list = extract_clauses(expr)
-#     wrong_clauses = filter(i -> occursin("∧",string(i)), clause_list)
-#     if !isempty(wrong_clauses)
-#         clause_list = distribute_and_over_or_recursively!(expr)
-#     end
-
-#     return unique!(clause_list)
-# end
-
-# """
-#     replace_logic_operators!(expr)
-
-# Replace ∨ for +; replace ¬ for 1 - var.
-# """
-# function replace_logic_operators!(expr)
-#     if expr isa Expr
-#         if expr.args[1] == :∨
-#             expr.args[1] = :(+)
-#         elseif expr.args[1] == :¬
-#             A = expr.args[2]
-#             expr = :(1 - $A)
-#         end
-#         for i in 2:length(expr.args)
-#             expr.args[i] = replace_logic_operators!(expr.args[i])
-#         end
-#     end
-
-#     return expr
-# end
