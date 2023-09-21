27b95c9aef748df91cb848b1c376842c96db0717
Hector Perez <hperez16@gmail.com>
2023-09-21 10:27:20 -0400

    Update src/reformulate.jl
    Co-authored-by: Joshua Pulsipher <33848311+pulsipher@users.noreply.github.com>

--- src/reformulate.jl
+++ src/reformulate.jl
@@ -75,7 +75,7 @@ end
     ) where {T<:Disjunction}
 
 Reformulate a disjunction using the specified `method`. Current reformulation methods include
-`BigM`, `Hull`, and `Indicator`. This method can be extended for other reformualtion techniques.
+`BigM`, `Hull`, and `Indicator`. This method can be extended for other reformulation techniques.
 
 The `disj` field is the `ConstraintData` object for the disjunction, stored in the 
 `disjunctions` field of the `GDPData` object.
