e349c4b1c7fd1276c595487580b8c80adf38a324
pulsipher <joshuapulsipher13@gmail.com>
2023-09-21 10:56:43 -0400

    remove files that shouldn't be git tracked

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#Overview","page":"About","title":"Overview","text":"","category":"section"},{"location":"","page":"About","title":"About","text":"JuGDP.jl","category":"page"}]
-}
