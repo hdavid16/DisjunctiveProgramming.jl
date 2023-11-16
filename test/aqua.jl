08878e35e1943b5a8aa1562ef9a748b70384324b
Hector Perez <hperez16@gmail.com>
2023-11-16 12:42:24 -0500

    fix aqua test

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,5 +1,6 @@
 using Aqua
 using DisjunctiveProgramming
 
-Aqua.test_all(DisjunctiveProgramming, ambiguities = false)
+Aqua.test_all(DisjunctiveProgramming, deps_compat = false, ambiguities = false)
+Aqua.test_deps_compat(DisjunctiveProgramming, check_extras = (ignore=[:Test, :HiGHS],))
 Aqua.test_ambiguities(DisjunctiveProgramming)
